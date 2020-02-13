6ff1f0c92d47398442b7a5538f497c381e5c9315
Manjunath2017 <manjunath.jadhav1231@gmail.com>
2020-02-13 18:34:00 +0530

    post model, add a post on timeline

--- router/api/posts.js
+++ router/api/posts.js
@@ -1,7 +1,41 @@
 const express=require('express');
-
 const router=express.Router();
 
-router.get('/', (req,res)=>{ res.send('Post Module!')})
+const postModel=require('../../models/post');
+const profileModel=require('../../models/profile');
+const userModel=require('../../models/users');
+
+const {check, validationResult}=require('express-validator');
+const auth =require('../../middleware/auth');
+
+
+router.post('/',  [auth, [
+    check('text', 'text is required!').not().isEmpty()
+]
+], async(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty){
+        return res.status(400).json({error:errors.array() });
+    }
+    console.log(res.authUserData.id, 'auth id');
+    try{
+        const user=await userModel.findById(res.authUserData.id).select('-password');
+        const newPost=new postModel({
+            text:req.body.text,
+            name:user.name,
+            avatar:user.avatar,
+            user:user.id
+        });
+        console.log(user, newPost);
+
+        const post=await newPost.save();
+        res.json(post);
+        
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send('Server error, Post');
+    }
+
+});
 
 module.exports=router;
