4768e68d10af2ff24b302ddd2c5b182d26b38ec9
Manjunath2017 <manjunath.jadhav1231@gmail.com>
2020-02-16 22:41:30 +0530

    add router, create auth/login and auth/register component, made use of anchor tag(Link)

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Landing from './components/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 
 const App=()=> (
+  <Router>
   <Fragment>
     <NavBar />
-    <Landing />
+    <Route exact path='/' component={Landing} />
+    <section className="container">
+      <Switch>
+        <Route exact path='/login' component={Login} />    
+        <Route exact path='/register' component={Register} />    
+      </Switch>
+    </section>
   </Fragment>
+  </Router>
 );
 
 export default App;
